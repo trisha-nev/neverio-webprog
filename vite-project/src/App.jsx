851189b4ee1a0258b62b4690b57fef32b2b57095
trisha-nev <torinvr@gmail.com>
2026-03-11 13:36:48 +0800

    lab-act1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,32 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <header className='App-header'>
+          <h1>Welcome to my React App!</h1>
+          <p>
+            Name: Trisha Neverio <br />
+            Email: neveriotl@national-u.eduph <br />
+            Section: INF231 
+          </p>
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
