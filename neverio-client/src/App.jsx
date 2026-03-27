3a1dc576afc7dca6d7f2dd3e1dbe653f466e8b81
trisha-nev <torinvr@gmail.com>
2026-03-28 00:38:08 +0800

    lab-act-1

---
+++ neverio-client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Welcome to my React App!</h1>
+        <p>
+          Name: Trisha Neverio<br  />
+          Email: neveriotl@students.national-u.edu.ph<br  />
+          Section: INF231<br  />
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App
